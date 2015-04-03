f4e03531c2c219c5a267d5e71a27bba4b33b644d
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-04-03 10:18:19 +0200

    ui updated

--- app/ui.R
+++ app/ui.R
@@ -1,5 +1,24 @@
 shinyUI(fluidPage(
   titlePanel("Bioinformatics jobs"),
   
-  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Choose the link you want job you want to see"),
+      
+      selectInput("pageItem", 
+                  label = "Select the job:",
+                  choices = c(1:10),
+                  selected = 1),
+      
+      
+      actionButton(inputId = "prevPage",
+                   label = "Prev"),
+      actionButton(inputId = "nextPage",
+                   label = "Next")
+    ),
+    
+    mainPanel(
+      uiOutput("xmlOutput")
+    )
+  )
 ))
