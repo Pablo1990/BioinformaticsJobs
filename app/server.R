3d6654266142d1ad50f0ba465a5737cafb17816e
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-04-03 10:18:31 +0200

    trying to work this out

--- app/server.R
+++ app/server.R
@@ -12,7 +12,19 @@ for (item in feedItems) {
   print(xmlChildren(item)$title)
 }
 
+cont <- 1
+
 shinyServer(function(input, output) {
   #This code executes the user visits the site
+  output$xmlOutput <- renderUI({
+    if(cont!=0){
+      if(input$prevPage){
+        cont <- cont + 10
+      } else {
+        cont <- cont - 10
+      }
+    }
+    paste(xmlChildren(feedItems[input$pageItem + cont]))
+  })
   
 })
