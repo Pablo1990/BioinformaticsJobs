9520e1afcf7e5f3b54c9362bb03311011e1d2f49
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-04-04 23:29:25 +0200

    init personal page

---
+++ personal/server.R
@@ -0,0 +1,7 @@
+#server.R
+#This code executes the app is launched.
+
+shinyServer(function(input, output) {
+  #This code executes the user visits the site
+  
+})
---
+++ personal/ui.R
@@ -0,0 +1,12 @@
+shinyUI(fluidPage(
+  titlePanel("Pablo Vicente Munuera"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Choose the link you want job you want to see")
+    ),
+    
+    mainPanel(
+    )
+  )
+))
