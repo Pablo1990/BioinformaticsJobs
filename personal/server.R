ce0cf5a397846e16dbc5f01b0422b5a8d5080097
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-04-05 22:16:42 +0200

    Merge with the bioinformatics jobs

--- personal/server.R
+++ personal/server.R
@@ -1,8 +1,26 @@
 #server.R
 #This code executes the app is launched.
 
+library("XML")
+
+#feed <- xmlTreeParse(file = "http://www.inab.org/category/job-opportunities/feed/", isURL = TRUE)
+#feed <- xmlTreeParse(file = "http://www.mathjobs.org/jobs?joblist-0-----rss", isURL = TRUE)
+feed <- xmlTreeParse(file = "http://www.madrimasd.org/informacionidi/noticias/rss/empleo.ashx", isURL = TRUE)
+
+feedItems <- xmlElementsByTagName(feed$doc$children$rss, name = "item", recursive = TRUE)
+
+#xmlChildren(feedItems[1]$channel.item)
+
+# for (item in feedItems) {
+#   print(xmlChildren(item)$title)
+# }
+
+siguientePage <- 0
+
 shinyServer(function(input, output) {
   #This code executes the user visits the site
+  
+  #------------------- Personal page code ---------------------#
   output$h1 <- renderUI({
     h1(input$whatYouSee)
   })
@@ -16,6 +34,7 @@ shinyServer(function(input, output) {
         Open mind and open source (when it's possible). 
         Love impressive presentations. Always looking for challenges. 
         Traveling is ok.")
+      #maybe profiles of other webs here?
     }
     else if(input$whatYouSee == "Skills"){
       HTML("<i>Programming languages:</i>
@@ -37,8 +56,93 @@ shinyServer(function(input, output) {
     }else if(input$whatYouSee == "Languages"){
       HTML("Spanish native and English proficient (Certificated by Duolingo).")
       #programming languages here?
-    }else{
-      HTML("")
+    }else if(input$whatYouSee == "Education"){
+      HTML("<p>I'd like you to tell a <i>story</i>... 
+           When I was a young boy (around 17s)</p>")
+    }else {
+      HTML("HackingMedicine Madrid, Hackathon El Mundo, ...")
+      #Events assited, other relevant info
     }
+  })
+  
+  #----------------- Jobs code --------------------#
+  cont <- 0
+  contAnt <- cont
+  selectedItem <- 1
+  aux <- 1
+  prevPage <- 0
+  
+  #aux <- as.integer(input$pageItem) + cont
+  #print(xmlChildren(feedItems[aux]$channel.item))
+  listItem <- xmlChildren(feedItems[aux]$channel.item)
+  
+  
+  updateListFeed <- function (input, cont, feedItems) {
+    if(!is.null(input$cbItems))
+      aux <- as.integer(input$cbItems) + cont
+    else
+      aux <- 1
+    #print(xmlChildren(feedItems[aux]$channel.item))
+    listItem <- xmlChildren(feedItems[aux]$channel.item)
+    
+    #return (listItem)
+  }
+  
+  output$cbItems <- renderUI({
+    #     if(input$nextPage != siguientePage){
+    #       siguientePage = input$nextPage[1]
+    #       
+    #       contAnt <- cont
+    #       cont <- cont + 10
+    #       if(cont>=length(feedItems)){
+    #         cont <- contAnt
+    #       }
+    #     }
+    #     if(input$prevPage != prevPage){
+    #       prevPage <- prevPage + 1
+    #       
+    #       contAnt <- cont
+    #       cont <- cont - 10
+    #       if(cont<0){
+    #         cont <- contAnt
+    #       }
+    #     }
+    #print(cont)
+    #     if(contAnt==cont)
+    #       if(!is.null(input$cbItems))
+    #         selectedItem <- as.integer(input$cbItems)
+    #     else
+    #       selectedItem <- cont + 1
+    #     
+    #     if(cont+10<length(feedItems)){
+    #       selectInput("cbItems", label = "Select job:", selected = selectedItem, choices = (cont+1):(cont+10))
+    #     }else{
+    selectInput("cbItems", label = "Select job:", selected = 1, choices = (cont+1):(length(feedItems)))
+    #     }
+  })
+  
+  
+  output$xmlTitle <- renderUI({
+    listItem <- updateListFeed(input, cont, feedItems)
+    title2 <- xmlValue(listItem$title)
+    h4(title2)
+  })
+  
+  output$xmlDescription <- renderUI({
+    listItem <- updateListFeed(input, cont, feedItems)
+    description <- xmlValue(listItem$description)
+    p(description)
+  })
+  
+  output$xmlInfo <- renderUI({
+    listItem <- updateListFeed(input, cont, feedItems)
+    info <- xmlValue(listItem$encoded)
+    p(info)
+  })
+  
+  output$xmlPubDate <- renderUI({
+    listItem <- updateListFeed(input, cont, feedItems)
+    pubDate <- xmlValue(listItem$pubDate)
+    p(pubDate)
   })
 })
--- personal/ui.R
+++ personal/ui.R
@@ -1,18 +1,37 @@
-shinyUI(fluidPage(
-  titlePanel("Pablo Vicente Munuera"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      helpText("This is my personal page"),
-      #img(src = "avatar.JPG", heigth = '50%', width = '50%'),
-      radioButtons("whatYouSee",label = "What do you want to know about me?", 
-                   choices = list("Profile" = "Profile", "Skills" = "Skills", 
-                                  "Education" = "Eduction", "Languages" = "Languages"))
-    ),
-    #http://shiny.rstudio.com/gallery/including-html-text-and-markdown-files.html
-    mainPanel(
-      uiOutput(outputId = "h1"),
-      uiOutput(outputId = "outputText")
-    )
+shinyUI(
+  navbarPage("Pablo Vicente Munuera",
+             tabPanel("Personal page", 
+                      sidebarLayout(
+                        sidebarPanel(
+                          helpText("This is my personal page"),
+                          #img(src = "avatar.JPG", heigth = '50%', width = '50%'),
+                          radioButtons("whatYouSee",label = "What do you want to know about me?", 
+                                       choices = list("Profile" = "Profile", "Skills" = "Skills", 
+                                                      "Education" = "Education", "Languages" = "Languages"
+                                                      , "Other" = "Other"))
+                        ),
+                        #http://shiny.rstudio.com/gallery/including-html-text-and-markdown-files.html
+                        mainPanel(
+                          uiOutput(outputId = "h1"),
+                          uiOutput(outputId = "outputText")
+                        )
+                      )
+             ),
+             tabPanel("Jobs page",
+                      sidebarLayout(
+                        sidebarPanel(
+                          helpText("Choose the link you want job you want to see"),
+                          
+                          uiOutput("cbItems")
+                        ),
+                        
+                        mainPanel(
+                          uiOutput("xmlTitle"),
+                          uiOutput("xmlDescription"),
+                          uiOutput("xmlInfo"),
+                          uiOutput("xmlPubDate")
+                        )
+                      )
+             )
   )
-))
+)
