8315fea0b3596d4becaffa2f7e1129386f36334a
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-04-05 21:06:16 +0200

    radioButton and filling the tabs

--- personal/server.R
+++ personal/server.R
@@ -3,5 +3,42 @@
 
 shinyServer(function(input, output) {
   #This code executes the user visits the site
+  output$h1 <- renderUI({
+    h1(input$whatYouSee)
+  })
   
+  output$outputText <- renderUI({
+    if(input$whatYouSee == "Profile"){
+      HTML("Looking for a career on bioinformatics. 
+        Trying to help the environment. Active in social media.
+        Not enough knowledge of biology, but trying to improve it.
+        Honest and hard work. Really love system biology. 
+        Open mind and open source (when it's possible). 
+        Love impressive presentations. Always looking for challenges. 
+        Traveling is ok.")
+    }
+    else if(input$whatYouSee == "Skills"){
+      HTML("<i>Programming languages:</i>
+<ul>
+  <li>R</li>
+	<li>Java</li>
+	<li>Python</li>
+	<li>Matlab</li>
+	<li>C/C++</li>
+	<li>HTML5/Javascript/Php</li>
+</ul>
+<i>Programming tools:</i>
+<ul>
+	<li>Git</li>
+	<li>SVN</li>
+	<li>General IDEs: Eclipse, netbeans, Rstudio, ...</li>
+</ul>
+")
+    }else if(input$whatYouSee == "Languages"){
+      HTML("Spanish native and English proficient (Certificated by Duolingo).")
+      #programming languages here?
+    }else{
+      HTML("")
+    }
+  })
 })
--- personal/ui.R
+++ personal/ui.R
@@ -3,10 +3,16 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Choose the link you want job you want to see")
+      helpText("This is my personal page"),
+      #img(src = "avatar.JPG", heigth = '50%', width = '50%'),
+      radioButtons("whatYouSee",label = "What do you want to know about me?", 
+                   choices = list("Profile" = "Profile", "Skills" = "Skills", 
+                                  "Education" = "Eduction", "Languages" = "Languages"))
     ),
-    
+    #http://shiny.rstudio.com/gallery/including-html-text-and-markdown-files.html
     mainPanel(
+      uiOutput(outputId = "h1"),
+      uiOutput(outputId = "outputText")
     )
   )
 ))
