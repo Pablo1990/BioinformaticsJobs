d76feab7b55df3742aa6cb3caa81a262138b8b36
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-04-15 13:37:56 +0200

    updating cv

--- index.R
+++ index.R
@@ -1,5 +1,7 @@
 library(shiny)
 #runApp("Test", display.mode = "showcase")
-#runApp("personal")
-#runGitHub( "bioinformaticsJobs", "Pablo1990")
+runApp("personal")
+#runGitHub(repo="BioinformaticsJobs", name = "Pablo1990", subdir = 'personal')
 #run
+#library(shinyapps)
+#shinyapps::deployApp('personal')
--- personal/server.R
+++ personal/server.R
@@ -3,24 +3,6 @@
 
 library("XML")
 
-feed <- xmlTreeParse(file = "http://www.inab.org/category/job-opportunities/feed/", isURL = TRUE)
-#feed2 <- xmlTreeParse(file = "http://www.mathjobs.org/jobs?joblist-0-----rss", isURL = TRUE)
-feedItemsInab <- xmlElementsByTagName(feed$doc$children$rss, name = "item", recursive = TRUE)
-
-feed <- xmlTreeParse(file = "http://www.madrimasd.org/informacionidi/noticias/rss/empleo.ashx", isURL = TRUE)
-feedItemsMadrid <- xmlElementsByTagName(feed$doc$children$rss, name = "item", recursive = TRUE)
-
-url <- "http://www.bioinformatics.org/jobs/?group_id=101&summaries=1"
-doc <- htmlTreeParse(url, useInternalNodes = T)
-
-links <- xpathSApply(doc, "//a[contains(@href, 'forums')]", xmlAttrs)
-#the first item is useless
-linksNames <- xpathSApply(doc, "//a[contains(@href, 'forums')]", xmlValue)
-
-#siguientePage <- 0
-
-remove(feed)
-
 shinyServer(function(input, output) {
   #This code executes the user visits the site
   
@@ -63,12 +45,44 @@ shinyServer(function(input, output) {
 </ul>
 ")
     }else if(input$whatYouSee == "Hobbies"){
-      HTML("Basketball, Monica")
+      HTML("<h3>Free time</h3>
+
+I enjoy my life day by day. Walking around with my girlfriend, have a dinner with her, or just being with her, it's a pleasure. I also enjoy listening music, reading books and playing sports. I love programming and i'm a passionate of my profession or what I do for living.
+
+<h3>Music</h3>
+
+Checkout my profile on spotify: <a href=\"https://open.spotify.com/user/pablo_1990\">profile</a>.
+
+<h3>Sports</h3>
+
+My sports is basketball, i really love play basket. But I enjoy play any kind of sports: football, tennis, running, walking, ...
+
+<h3>Books</h3>
+
+Checkout my profile on goodreads: <a href=\"https://www.goodreads.com/user/show/15879081-pablo-vicente-vicente\">profile</a>.")
     }else if(input$whatYouSee == "Education"){
       HTML("<p>I'd like you to tell a <i>story</i>... 
            When I was a young boy (around 17s)</p>")
     }else {
-      HTML("HackingMedicine Madrid, Hackathon El Mundo, ...")
+      HTML("<h3>Events</h3>
+
+I've assited to some hack-a-thon-s in the last years: Hackathon \"El Mundo\" (<a href=\"https://github.com/Pablo1990/BioinfoTeam\">link</a>), Hacking Medicine Madrid (<a href=\"https://github.com/rafassmail/hackMadrid\">link</a>) and spaceApps by \"NASA\" (<a href=\"https://2015.spaceappschallenge.org/project/chic-communication-human-imagination-creativity-advertising/\">link</a>).
+
+<h3>Personal projects</h3>
+
+In my university years, i've contributed to several projects (solo or group-based):<br/>
+<i>Group-based</i>
+<ul>
+  <li><a href=\"https://github.com/Pablo1990/3DVideogameABPSlayers\">Slayers</a>: 3D videogame</li>
+	<li><a href=\"https://github.com/fawques/CodeSpain\">Codespain</a>: It's a web app to let developer find easily developer events in Spain.</li>
+	<li><a href=\"https://github.com/fawques/SOR\">SOR project</a>: </li>
+</ul>
+
+<i>Solo projects</i>
+<ul>
+	<li><a href=\"https://github.com/Pablo1990/BioinformaticsJobs\">bioinfoJobs</a>:</li>
+	<li><a href=\"https://github.com/Pablo1990/BeThePerfectTraveller\">BeThePerfectTraveller</a>: Select the country you want to know and the app will show you what is the percentege you have to pay (or what is recommended) and an advice if you travell to that country.</li>
+</ul>")
       #Events assited, other relevant info
     }
   })
@@ -79,6 +93,22 @@ shinyServer(function(input, output) {
   selectedItem <- 1
   #aux <- 1
   prevPage <- 0
+  
+  feed <- xmlTreeParse(file = "http://www.inab.org/category/job-opportunities/feed/", isURL = TRUE)
+  #feed2 <- xmlTreeParse(file = "http://www.mathjobs.org/jobs?joblist-0-----rss", isURL = TRUE)
+  feedItemsInab <- xmlElementsByTagName(feed$doc$children$rss, name = "item", recursive = TRUE)
+  
+  feed <- xmlTreeParse(file = "http://www.madrimasd.org/informacionidi/noticias/rss/empleo.ashx", isURL = TRUE)
+  feedItemsMadrid <- xmlElementsByTagName(feed$doc$children$rss, name = "item", recursive = TRUE)
+  
+  url <- "http://www.bioinformatics.org/jobs/?group_id=101&summaries=1"
+  doc <- htmlTreeParse(url, useInternalNodes = T)
+  
+  links <- xpathSApply(doc, "//a[contains(@href, 'forums')]", xmlAttrs)
+  #the first item is useless
+  linksNames <- xpathSApply(doc, "//a[contains(@href, 'forums')]", xmlValue)
+  
+  #siguientePage <- 0
   
   #aux <- as.integer(input$pageItem) + cont
   #print(xmlChildren(feedItems[aux]$channel.item))
